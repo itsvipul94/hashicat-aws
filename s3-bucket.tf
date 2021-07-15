074906c32440d092319b00dd1d757711331fd369
vipul <itsvipul94@gmail.com>
2021-07-15 21:49:19 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 
-module "s3-bucket_example_complete" {
+module "bucket_example_complete" {
   source  = "app.terraform.io/VipulWaibhav-training/s3-bucket/aws//examples/complete"
   version = "2.2.0"
   bucket_prefix = "${var.prefix}"
